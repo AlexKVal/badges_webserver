357fd24dea63a97bb7d4115ade1cc608bb686617
AlexKVal <alexkval@gmail.com>
2015-03-21 20:02:03 +0200

    add styles

--- public/js/main.js
+++ public/js/main.js
@@ -7,5 +7,8 @@ $(function () {
   socket.on('badge', function (badge) {
     var $img = $('<img src="'+badge.badge_id+'" alt="Code School Badge">');
     $('.badge-wrapper').prepend($img);
+    setTimeout(function(){
+      img.addClass('on');
+    }, 0);
   });
 });
