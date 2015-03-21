d1740fc263836df88f98bc730d4764668730f914
AlexKVal <alexkval@gmail.com>
2015-03-21 18:38:47 +0200

    server listening on 3000 port

--- app.js
+++ app.js
@@ -3,4 +3,12 @@
 var express = require('express');
 var app = express();
 
+/**
+ * Server static assets out of public
+ */
 app.use(express.static('public'));
+
+var port = 3000;
+app.listen(port, function () {
+  console.log('Server is listening on port %d', port);
+});
